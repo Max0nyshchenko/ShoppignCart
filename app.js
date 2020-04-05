6e57317a3e436e4d8357159e9e51bc5edefa3f6e
Max Onyshchenko <max.0nyshenk03@gmail.com>
2020-04-06 02:08:41 +0300

    add styling

---
+++ app.js
@@ -0,0 +1,9 @@
+// vars
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+
