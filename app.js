74f0ca97fb11ee669c72484d252e3a3f3ab3bfc7
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-04-24 19:10:28 +0300

    improving site workspeed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: 'mp37789ggtdw',
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: 'LvTsozlBJfRZJnmos7cipnlCVrePgNvv2j74li7W8d8',
-});
+// const client = contentful.createClient({
+//   // This is the space ID. A space is like a project folder in Contentful terms
+//   space: 'mp37789ggtdw',
+//   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+//   accessToken: 'LvTsozlBJfRZJnmos7cipnlCVrePgNvv2j74li7W8d8',
+// });
 
 // vars
 
